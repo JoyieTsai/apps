bed91dc67865234ad8d9f306b51f4fecbc18c8a7
Joyie Tsai <>
2019-04-02 11:45:15 +0800

    deploy

---
+++ precache-manifest.56cfb086a1ef19bdfc8b3d93c64b337a.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = [
+  {
+    "revision": "73c94a52932ae4a3b21078591c222b2c",
+    "url": "img/InfoChatting-asset-03.73c94a52.png"
+  },
+  {
+    "revision": "6eb8605530ddd9261e622d2d94058523",
+    "url": "static/file/InfoVNS_mapping-20181030200632/iOS/InfoVNS_mapping/VW_2018.10.30.ipa"
+  },
+  {
+    "revision": "2649a5ae021415c36a277ca4509ee433",
+    "url": "static/file/InfoVNS_mapping-20181030200632/iOS/InfoVNS_mapping/VW_2018.10.30.plist"
+  },
+  {
+    "revision": "11bada7ea99c1fdad6491b979cda8737",
+    "url": "img/03.11bada7e.png"
+  },
+  {
+    "revision": "40e105c8fc9e1469f5b8",
+    "url": "js/chunk-vendors.16f35901.js"
+  },
+  {
+    "revision": "c6deb49102451c8a3174e74219668fa5",
+    "url": "static/file/InfoVNS_mapping-20181030200632/Android/InfoVNS_mapping/VWPortal_dev_2018.10.30.apk"
+  },
+  {
+    "revision": "c60e5e44dadfcc643582c24e8b4a6f63",
+    "url": "static/file/InfoTracker/Android/InfoTracker_live_2017.09.27.apk"
+  },
+  {
+    "revision": "f8dbc39207aa78e38fefdd7f81443b26",
+    "url": "static/file/InfoChatting/Android/InfoChatting_live_2017.02.24.apk"
+  },
+  {
+    "revision": "b5f21d19e0c6678f84e613b2de34770d",
+    "url": "static/file/InfoVNS_mapping-20181030200632/iOS/InfoVNS_mapping/Icon.png"
+  },
+  {
+    "revision": "b5f21d19e0c6678f84e613b2de34770d",
+    "url": "static/file/InfoVNS_mapping-20181030200632/Android/InfoVNS_mapping/Icon.png"
+  },
+  {
+    "revision": "efa5fef59d8577ad1dac0569b1868128",
+    "url": "static/file/Genie/Android/Genie_live_2017.06.02.apk"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cecb1387d8db697e3672d8ea2d4aab3b",
+    "url": "img/01.cecb1387.jpg"
+  },
+  {
+    "revision": "045ac9cbb0410b59af6e6520daf5fb88",
+    "url": "img/01.045ac9cb.jpg"
+  },
+  {
+    "revision": "a525e48471147e86870798f4e37ec54e",
+    "url": "img/Genie-appimg-02.a525e484.png"
+  },
+  {
+    "revision": "283c9240a8cae048d5cf80282050bdef",
+    "url": "img/InfoChatting.283c9240.png"
+  },
+  {
+    "revision": "4a899048b953a131ebe129d0019958ae",
+    "url": "img/02.4a899048.jpg"
+  },
+  {
+    "revision": "cc21345b419a57e03b32306d44f5562e",
+    "url": "img/02.cc21345b.jpg"
+  },
+  {
+    "revision": "eb106c3318d0331bbddeb714edb140f1",
+    "url": "img/InfoTracker.eb106c33.png"
+  },
+  {
+    "revision": "b5f21d19e0c6678f84e613b2de34770d",
+    "url": "img/InfoVNS.b5f21d19.png"
+  },
+  {
+    "revision": "6fc345bd1a5b2666e7788982752accae",
+    "url": "img/pattern.6fc345bd.png"
+  },
+  {
+    "revision": "5594351a24ab72630cc0ea5aec7fc18b",
+    "url": "img/landing-img.5594351a.png"
+  },
+  {
+    "revision": "e96739ec0a87b37cee0c1f6c5260667c",
+    "url": "img/04.e96739ec.jpg"
+  },
+  {
+    "revision": "9f0c5d4c2e14e2b6710f3cd5c28975de",
+    "url": "img/04.9f0c5d4c.png"
+  },
+  {
+    "revision": "ae4dbede9d81a20c842b30332ab4e5b6",
+    "url": "img/materialdesignicons-webfont.ae4dbede.svg"
+  },
+  {
+    "revision": "b9cfca803ac3184792f6681a3ea0da81",
+    "url": "img/04.b9cfca80.jpg"
+  },
+  {
+    "revision": "a6a9ee17bb02b2a9b919a27520dbaa78",
+    "url": "img/Genie.a6a9ee17.png"
+  },
+  {
+    "revision": "ed88596dca1ae1c84f86c08859e2ebc6",
+    "url": "img/03.ed88596d.jpg"
+  },
+  {
+    "revision": "9c6943f1566039a7414f1df7437db7ba",
+    "url": "img/01.9c6943f1.jpg"
+  },
+  {
+    "revision": "122e0fda58a86c85f4f7248a44e69f35",
+    "url": "img/05.122e0fda.jpg"
+  },
+  {
+    "revision": "20809e339335097c5d562ad70f9f1a7e",
+    "url": "img/02.20809e33.jpg"
+  },
+  {
+    "revision": "8f1a92639a74a89ca31475e338b67df7",
+    "url": "img/03.8f1a9263.jpg"
+  },
+  {
+    "revision": "a303c97ac9761b900e424dc72561425b",
+    "url": "img/01.a303c97a.png"
+  },
+  {
+    "revision": "e1228ff12dd84beee82d2a2de402c6e2",
+    "url": "img/InfoTracker-appimg-04.e1228ff1.jpg"
+  },
+  {
+    "revision": "8207d53f90c59af3be2035d2b2aa85a3",
+    "url": "img/Rolmes.8207d53f.png"
+  },
+  {
+    "revision": "e111c54218eb54e769e31b6476f385a3",
+    "url": "img/03.e111c542.png"
+  },
+  {
+    "revision": "d4fbafb42babd0c82e6218f16ae90cad",
+    "url": "img/06.d4fbafb4.jpg"
+  },
+  {
+    "revision": "5189ea9e2435ddc9e772",
+    "url": "js/app.a83f8b47.js"
+  },
+  {
+    "revision": "a0cfdc6bae713901d86dd3e34a3eef01",
+    "url": "img/InfoTracker-appimg-05.a0cfdc6b.jpg"
+  },
+  {
+    "revision": "addd67d07323f1a31f574b5b9d9f7279",
+    "url": "img/InfoChatting-appimg-01.addd67d0.png"
+  },
+  {
+    "revision": "6395990bb304365cd4bc5a5dde30c372",
+    "url": "img/InfoChatting-asset-02.6395990b.png"
+  },
+  {
+    "revision": "f6195d70a50ad62a632cbeedd3061fe1",
+    "url": "img/04.f6195d70.jpg"
+  },
+  {
+    "revision": "42b9f7cbbc9f69eee9da4aa6c1f8ff88",
+    "url": "img/InfoTracker-appimg-01.42b9f7cb.jpg"
+  },
+  {
+    "revision": "470d6aafc5f6f13d9d59d5bd4d947fdb",
+    "url": "img/02.470d6aaf.jpg"
+  },
+  {
+    "revision": "93af2b2d4f05e1ece6e323675051e4a4",
+    "url": "img/InfoChatting-asset-01.93af2b2d.png"
+  },
+  {
+    "revision": "6451125dce5caa9837223355e78fa0db",
+    "url": "img/02.6451125d.png"
+  },
+  {
+    "revision": "fe4059904ca83aca063a6c433b7e55bb",
+    "url": "img/03.fe405990.jpg"
+  },
+  {
+    "revision": "2a4c0695d93bad37fee218962e03fbc4",
+    "url": "img/05.2a4c0695.jpg"
+  },
+  {
+    "revision": "75c25e77c1cd9015cce30e948ca63689",
+    "url": "img/01.75c25e77.jpg"
+  },
+  {
+    "revision": "94842fac95f5825d7e84fea67fa0fe21",
+    "url": "img/brand-logo.94842fac.png"
+  },
+  {
+    "revision": "e0df6a3df416bef4fb89ae0cddcf48b7",
+    "url": "img/InfoAlert.e0df6a3d.png"
+  },
+  {
+    "revision": "1118714c78d5733b70c529cfbebf2a16",
+    "url": "img/Personnel.1118714c.png"
+  },
+  {
+    "revision": "12ece5a16b3b184591e4a52bd2bb8042",
+    "url": "img/01.12ece5a1.jpg"
+  },
+  {
+    "revision": "71d21e0fd50df70dc9ab23431dcf914f",
+    "url": "img/InfoTracker-appimg-03.71d21e0f.jpg"
+  },
+  {
+    "revision": "ffa6906aec21979e04569568c5a713bd",
+    "url": "img/04.ffa6906a.jpg"
+  },
+  {
+    "revision": "835a0e076681667a3f2b2122c481b10f",
+    "url": "img/02.835a0e07.jpg"
+  },
+  {
+    "revision": "e48b0539ff6cf32d1c322991b429733b",
+    "url": "img/03.e48b0539.jpg"
+  },
+  {
+    "revision": "e0275cb7767fadac1fa0088f924490da",
+    "url": "img/05.e0275cb7.png"
+  },
+  {
+    "revision": "048e76f6341dc348ca6d0b2f4d8ce4d3",
+    "url": "img/Genie-appimg-01.048e76f6.png"
+  },
+  {
+    "revision": "c4dcf055c7212191e69e555990b93182",
+    "url": "img/01.c4dcf055.png"
+  },
+  {
+    "revision": "ebbd043fff4bcabbbcec7097bbaf113b",
+    "url": "img/InfoChatting-appimg-02.ebbd043f.png"
+  },
+  {
+    "revision": "7de6273d7bfe2a363f587d6e23fd6f90",
+    "url": "img/05.7de6273d.jpg"
+  },
+  {
+    "revision": "31233da3cc5625d57e21067c16faa850",
+    "url": "img/InfoTracker-appimg-02.31233da3.jpg"
+  },
+  {
+    "revision": "e7ff0ec5bdd4a57abee4860d478560dc",
+    "url": "img/InfoMobile.e7ff0ec5.png"
+  },
+  {
+    "revision": "f6dc339a7cc7d3e8385c5b3d9444b428",
+    "url": "index.html"
+  },
+  {
+    "revision": "d940e001103e1bb84893576658712911",
+    "url": "img/03.d940e001.jpg"
+  },
+  {
+    "revision": "3e78af6f1489488ec64e8ca98706fd15",
+    "url": "img/02.3e78af6f.png"
+  },
+  {
+    "revision": "02eae61dd3fa3e15b39987b75d7df827",
+    "url": "img/01.02eae61d.jpg"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "fonts/materialdesignicons-webfont.d15c1216.ttf"
+  },
+  {
+    "revision": "abb4e7492eaa88c31ff103064f2b2cba",
+    "url": "fonts/materialdesignicons-webfont.abb4e749.eot"
+  },
+  {
+    "revision": "4b882ad484d11b49f4a69212ad8e8901",
+    "url": "fonts/materialdesignicons-webfont.4b882ad4.woff"
+  },
+  {
+    "revision": "459118748df2a5fcd7f684546cc8591b",
+    "url": "fonts/materialdesignicons-webfont.45911874.woff2"
+  },
+  {
+    "revision": "638f8adb298df752da991f96403b61f2",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "40e105c8fc9e1469f5b8",
+    "url": "css/chunk-vendors.5a9e6b3d.css"
+  },
+  {
+    "revision": "5189ea9e2435ddc9e772",
+    "url": "css/app.265e8797.css"
+  }
+];
